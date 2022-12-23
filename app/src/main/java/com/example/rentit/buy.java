25134d5910d86375efa7f7e445e48589e3339300
Sumonta Saha Mridul <74852911+PromiMojumder38@users.noreply.github.com>
2022-12-23 12:49:22 +0600

    Intro Animated Screen Added at Start

---
+++ app/src/main/java/com/example/rentit/buy.java
@@ -0,0 +1,14 @@
+package com.example.rentit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+}
---
+++ app/src/main/java/com/example/rentit/rent.java
@@ -0,0 +1,14 @@
+package com.example.rentit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent);
+    }
+}
